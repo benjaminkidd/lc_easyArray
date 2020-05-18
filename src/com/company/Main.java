1a1c97afc426440269366fbd0ebc92c0be799f18
Benjamin Kidd <benkidd361@gmail.com>
2020-05-18 18:43:41 -0400

    return here if error

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,28 +17,42 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+//        String haystack = "hello";
+//        String needle = "ll";
         String haystack = "hello";
-        String needle = "ll";
+        String needle = "a";
 
-        assert(strStr(haystack,needle) == 2);
+        assert(strStr(haystack,needle) == -1);
     }
 
-    public static int strStr(String haystack, String needle){
+    public static int strStr(String haystack, String needle) {
         char[] hayArr = haystack.toCharArray();
         char[] needArr = needle.toCharArray();
         int hLength = haystack.length();
         int output = -1;
-        int j = 0;
-        if(needle.isEmpty()) {
+        if(haystack.isEmpty()){
+            output = -1;
+        }
+        else if (needle.isEmpty()) {
             output = 0;
+        }
+        else {
+            output = -1;
             for (int i = 0; i < hLength - 2; i++) {
+                int j = -1;
                 System.out.println("i = " + i);
-                if (hayArr[i] == needArr[j]) {
-                    output = i - j;
-                    j++;
+                if (hayArr[i] == needArr[j + 1]) {
+                    while (hayArr[i] == needArr[j + 1]) {
+                        output = i - j + 1;
+                        j++;
+                    }
+                }
+                else{
+                    output = j;
                 }
             }
         }
+
         System.out.println("temp " + output);
         return output;
     }
