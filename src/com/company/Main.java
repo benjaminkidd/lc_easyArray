77e2447dfa5d9eb6967bd402fe7303e6ca7f18e5
Benjamin Kidd <benkidd361@gmail.com>
2020-05-18 20:36:22 -0400

    working now to remove print statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -31,16 +31,24 @@ public class Main {
         int output = -1;
         output = haystack.isEmpty() ? -1 : -1;
         output = needle.isEmpty() ? 0 : -1;
+        char letter;
         int j = 0;
         for(int i = 0; i < haystack.length() - 1; i++) {
+            System.out.println("for i = " + i);
+            System.out.println("hay length = " + haystack.length());
             if(haystackArr[i] == needleArr[j]){
                 int k = i;
                 while(j < needle.length() - 1){
-                    output = haystackArr[k - j] == needleArr[j] ? needleArr[j] : -1;
-                    System.out.println(output);
+                    output = haystackArr[k - j] == needleArr[j] ? i : -1;
+                    System.out.println("k = " + k);
+                    System.out.println("i = " + i);
+                    System.out.println("haystack k - j = " + haystackArr[k-j]);
+                    System.out.println("needle j  = " + needleArr[j]);
+                    System.out.println("output = " + output);
                     j++;
                     k++;
                 }
+                j = 0;
             }
         }
         System.out.println("temp " + output);
